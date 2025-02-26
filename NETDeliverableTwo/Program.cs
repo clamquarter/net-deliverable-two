6769947c213c19b4afbca841b5bb1392d8670ab9
India Doria <indiadoria@gmail.com>
2025-02-26 16:45:06 -0500

    Declare program variables.

--- NETDeliverableTwo/Program.cs
+++ NETDeliverableTwo/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //.NET DELIVERABLE 2: The COIN FLIP CHALLENGE
 
+//Program Variables
+string userName = "User";
+int rndNum = 0;
+int score = 0;
+int roundNum = 0;
